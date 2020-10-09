b3ef1c0107556d5f4abab0560fe387741273376f
saifuullah <kk772578@gmail.com>
2020-10-09 17:36:40 +0500

    Merge changes

--- hello.py
+++ hello.py
@@ -7,11 +7,6 @@ print("TESt-1")
 
 print("HISTORY CHECKING")
 print("DIFF")
-
-<<<<<<< HEAD
-
-
 print("Addition from Owner's")
-=======
+
 print("ALi ki Line")
->>>>>>> 47e0b2f44fea168e89c253298a65d2794394d0f3
